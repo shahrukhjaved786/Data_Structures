31c968516410f033789af94edbeb35022c0f788d
shahrukhjaved786 <shahrukh.tjit14@gmail.com>
2024-01-30 00:07:20 +0530

    Problem Statement Correction

--- BruteForce/P2.1.1.2__2.py
+++ BruteForce/P2.1.1.2__2.py
@@ -1,6 +1,5 @@
 """
 Given an array, find the average of all contiguous sub-arrays of size ‘K’ in it.
-Let’s understand this problem with a real input:
 
 Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
 Output: [2.2, 2.8, 2.4, 3.6, 2.8]
